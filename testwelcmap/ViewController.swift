6284c2226d4e885dab372c3cfccbfd2559f136e0
Andrea <andrea@andreas-imac.lan>
2016-06-21 16:19:36 +0200

    Initial Commit

---
+++ testwelcmap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testwelcmap
+//
+//  Created by Andrea on 21/06/16.
+//  Copyright © 2016 SPAM Concept srl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
